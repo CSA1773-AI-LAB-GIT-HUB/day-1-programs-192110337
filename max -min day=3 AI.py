e6fdb5113ed33b6dbeb7971a76c5b2b7954a56b6
192110337 <112718533+192110337@users.noreply.github.com>
2022-09-21 10:59:10 +0530

    Add files via upload

---
+++ max -min day=3 AI.py
@@ -0,0 +1,9 @@
+# Python code to demonstrate the working of
+# max()
+
+# printing the maximum of 4,12,43.3,19,100
+print("Maximum of 4,12,43.3,19 and 100 is : ",end="")
+print (max( 4,12,43.3,19,100 ) )
+
+print("Minimum 0f 2,4,6,22.3,12 and 100 is :",end="")
+print(min(2,4,6,22.3,12,100))
